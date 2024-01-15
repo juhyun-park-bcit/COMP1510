50eac170304a6aaa10daaa88e6ec15900a72d644
JUHYUN PARK <jpark440@my.bcit.ca>
2024-01-14 23:03:28 -0800

    First Commit

--- Q1.py
+++ Q1.py
@@ -13,7 +13,6 @@ print(f"this is the volume: {volume}")
 double_radius = radius * 2
 surface_area_with_double_radius = 4 * pi * double_radius**2
 volume_double_radius = (4/3) * pi * double_radius**3
-
 # i
 print(f"The surface area with a doubled radius({double_radius}), is {surface_area_with_double_radius/surface_area} times as much as the surface area with a radius({radius}).")
 print(f"The volume with a doubled radius({double_radius}), is {volume_double_radius/volume} times as much as the volume with a radius({radius}).")
