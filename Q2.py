2c1bc62614c932277522f2db0506748ffb346e10
JUHYUN PARK <jpark440@my.bcit.ca>
2024-01-16 14:28:09 -0800

    Complete 2

--- Q2.py
+++ Q2.py
@@ -1,6 +1,3 @@
-import math
-
-
 # b
 coverage = 40
 # c, d, e, f
@@ -13,6 +10,6 @@ surface_area = (length_meters*width_meters) + 2*(length_meters*height_meters) + 
 total_coverage_needed = coats*surface_area
 cans_of_paint_required = total_coverage_needed / coverage
 # j
-the_number_of_cans = math.ceil(cans_of_paint_required)
+the_number_of_cans = -int(-(cans_of_paint_required) // 1)
 
 print(f"How many cans of paint they need to buy? {the_number_of_cans}")
